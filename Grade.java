a2294af3eb2b42ab11c5949a0619585d21be242e
davidojo1144 <davco1144@gmail.com>
2024-06-23 12:41:36 +0100

    assignment

---
+++ Grade.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Grade{
+        public static void main(String[] args){
+        Scanner input;
+	input = new Scanner(System.in);
+        int variable = 60;
+	int studentGrade = 61;
+
+	if (studentGrade >= 60);
+	System.out.println("passed");
+	
+	}
+	
+
+}
+
+
